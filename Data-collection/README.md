eb4b63d5be2c95604796d3330aeec7578158092d
hxuaj <31303673+hxuaj@users.noreply.github.com>
2019-04-11 16:19:22 +0800

    Add files via upload

--- Data-collection/README.md
+++ Data-collection/README.md
@@ -1,30 +1,32 @@
-# Image Data Collection
+# Data Collection
 
 ## Description
 
-This project is for Image/Video data collection from a camera which is plugged to a PC.
+This project is for Image/Video data collection from a camera which is plugged to a PC or a raspberry.
 
-Please set ```capture_fps```, ```i(pic saving freqency)```, ```rec_time```, ```resolution_scale```before capturing image data.
-The function will store video(.avi) and pictures(.jpg) simultaneously.
-Video is saved to "Data-video" folder and pictures are saved to "Data-pic" folder.
+The function will store video(.avi) and pictures(.jpg) simultaneously to different folders.
+
+Please set ```capture_fps```, ```i(pic saving freqency)```, ```rec_time```, ```resolution_scale```before capturing video and image data.
+
 
 ## Installation
 
 * Python 3 recommended
-    Python 2.7 will reach the end of its life on January 1st, 2020.
 * Library Dependencies:
     * numpy
     * opencv 3.4.1
 
 ## Usage
 
-1. Set up data collection environment.(indoor scenario)
-2. Connect a camera to a PC with USB or other interface.
-3. Run "data_collection.py" in powershell or cmd. (make sure PC will not auto lock)
+1. Set up data collection environment.
+2. Connect a camera to a PC or a raspberry pi with USB or CSI.
+3. Run "data_collection.py" in powershell or cmd.
     ```python
-    python .\data_collection.py
+    python .\data_collection.py [ -f <capture_fps>][-i <pic_save_freq>][-r <rec_time>][-rs <resolution_scale>]
     ```
-4. Find the data in file "Data-video" and "Data-pic".
+    Run ```python .\data_collection.py -h``` for help.
+
+4. Video is saved to "Data-video" folder and pictures are saved to "Data-pic" folder.
 
 ## License
 
--- Data-collection/data_collection.py
+++ Data-collection/data_collection.py
@@ -1,6 +1,21 @@
 # -*- coding: UTF-8 -*-
 
 import cv2
+from optparse import OptionParser
+
+
+usage = "data_collection.py[ -f <capture_fps>][-i <pic_save_freq>][-r <rec_time>][-rs <resolution_scale>]"
+opt = OptionParser(usage)
+opt.add_option("-f","--F",action = "store",type="int",dest = "capture_fps", 
+                default = 20, help = "Set the fps for video capturing.")
+opt.add_option("-i","--I",action = "store",type="int",dest = "pic_save_freq", 
+                default = 5, help = "Set the freq to save pictures.")
+opt.add_option("-r","--R",action = "store",type="int",dest = "rec_time", 
+                default = 30, help = "Record for rec_time seconds.")
+opt.add_option("-s","--S",action = "store",type="float",dest = "resolution_scale", 
+                default = 1.0, help = "Define the resolution_scale w.r.t. VGA.")
+
+options, args = opt.parse_args()
 
 # Start the default camera
 capture = cv2.VideoCapture(0)
@@ -10,18 +25,36 @@ print("The camera's default resolution is {} x {}".format(default_camera_width, 
 
 # Set capture FPS
 camera_fps = 20
-capture_fps = 20
+if not options.capture_fps:
+    capture_fps = camera_fps
+else:
+    capture_fps = options.capture_fps
+
 # Save a pic every i seconds
-i = 5
+if not options.pic_save_freq:
+    i = 5
+else:
+    i = options.pic_save_freq
+
 count_frame = 0
 pic_index = 0
 
 # Record for rec_time seconds
-rec_time = 30
+if not options.rec_time:
+    rec_time = 30
+else:
+    rec_time = options.rec_time
 
 # Define the codec and create VideoWriter object
-resolution_scale = 1/2
+if not options.resolution_scale:
+    resolution_scale = 1
+else:
+    resolution_scale = options.resolution_scale
 
+print("capture_fps = {}, pic_save_freq = {}, rec_time = {}, resolution_scale = {}".format(capture_fps, 
+                                                                                          i,
+                                                                                          rec_time,
+                                                                                          resolution_scale))
 capture_width = int(default_camera_width * resolution_scale)
 capture_height = int(default_camera_height * resolution_scale)
 
